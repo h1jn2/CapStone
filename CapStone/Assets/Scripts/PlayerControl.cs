da9bcd9b4c1451baf3ea86d4db23e1cb2e7fed3c
DESKTOP-IM2AR45\YJU <qkrtpgus11031210@gmail.com>
2024-03-27 13:51:09 +0900

    플레이어수정

--- CapStone/Assets/Scripts/PlayerControl.cs
+++ CapStone/Assets/Scripts/PlayerControl.cs
@@ -1,18 +1,15 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Fusion;
-using Unity.VisualScripting;
 
-public class PlayerMovement : NetworkBehaviour
+public class PlayerControl : MonoBehaviour
 {
-    public Camera Camera;
-
     [SerializeField]
     private float moveSpeed = 5f;
     [SerializeField]
     private float sprintSpeed = 10f;
+    [SerializeField]
+    private float mouseSpeed = 8f;
     [SerializeField]
     private float stamina = 100f;
     [SerializeField]
@@ -21,38 +18,42 @@ public class PlayerMovement : NetworkBehaviour
     private float deStamina = 20f;
     [SerializeField]
     private float reStamina = 10f;
-
     private float gravity;
     private CharacterController controller;
     private Vector3 move;
 
+
+    private float mouseX;
+    private float mouseY;
+
     private bool canSprint = true;
 
-    private void Awake()
+    // Start is called before the first frame update
+    void Start()
     {
         controller = GetComponent<CharacterController>();
         move = Vector3.zero;
         gravity = 10f;
     }
 
-    public override void FixedUpdateNetwork()
+    // Update is called once per frame
+    void Update()
     {
-        if (HasStateAuthority == false)
-        {
-            return;
-        }
-
         Control();
         Stamina();
     }
 
     private void Control()
     {
+        mouseX += Input.GetAxis("Mouse X") * mouseSpeed;
+        mouseY += Input.GetAxis("Mouse Y") * mouseSpeed;
+        this.transform.localEulerAngles = new Vector3(-mouseY, mouseX, 0);
+
         float currentSpeed = canSprint && Input.GetKey(KeyCode.LeftShift) ? sprintSpeed : moveSpeed;
 
         if (canSprint && currentSpeed == sprintSpeed)
         {
-            stamina -= deStamina * Runner.DeltaTime;
+            stamina -= deStamina * Time.deltaTime;
             stamina = Mathf.Clamp(stamina, 0f, maxStamina);
         }
 
@@ -63,35 +64,25 @@ public class PlayerMovement : NetworkBehaviour
         }
         else
         {
-            move.y -= gravity * Runner.DeltaTime;
+            move.y -= gravity * Time.deltaTime;
         }
 
-        controller.Move(move * currentSpeed * Runner.DeltaTime);
-
-        Debug.Log(stamina);
+        controller.Move(move * Time.deltaTime * currentSpeed);
     }
 
-    private void Stamina()
+    public void Stamina()
     {
         if (stamina <= 0)
         {
             canSprint = false;
         }
 
-        stamina += reStamina * Runner.DeltaTime;
+        stamina += reStamina * Time.deltaTime;
         stamina = Mathf.Clamp(stamina, 0f, maxStamina);
 
         if (stamina >= maxStamina)
         {
             canSprint = true;
-        }
-    }
-
-    public override void Spawned()
-    {
-        if (HasStateAuthority)
-        {
-            Camera = Camera.main;
         }
     }
 }
---
+++ CapStone/Assets/Scripts/Stamina.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Fusion;
+using UnityEngine;
+
+public class Stamina : NetworkBehaviour
+{
+    public PlayerMovement PlayerMovement;
+
+   
+}
