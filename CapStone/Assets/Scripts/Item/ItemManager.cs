a0b61c5c91a8fcd0ef53a139c78d18d532dcb6dc
SonJunhyeok <143947358+mikazuki5443@users.noreply.github.com>
2024-05-20 18:45:07 +0900

    로그인기능 작업

--- CapStone/Assets/Scripts/Item/ItemManager.cs
+++ CapStone/Assets/Scripts/Item/ItemManager.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 public class ItemManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private PhotonView pv;
     void Start()
     {
-        
+        pv = this.gameObject.GetComponent<PhotonView>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DestroyItem()
     {
         
     }
