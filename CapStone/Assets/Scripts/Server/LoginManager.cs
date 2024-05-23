0e0d34f350d485f7540c5bb342b763714ea33671
SonJunhyeok <143947358+mikazuki5443@users.noreply.github.com>
2024-05-23 17:50:57 +0900

    db접속및 보기 가능

--- CapStone/Assets/Scripts/Server/LoginManager.cs
+++ CapStone/Assets/Scripts/Server/LoginManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Xml;
 
 public class LoginManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private const string DB_TABLE_USER_LOGIN = "user";
     void Start()
     {
-        
+        view();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void view()
     {
-        
+        XmlNodeList userIdList = DBConnector.Select(DB_TABLE_USER_LOGIN);
+        foreach (XmlNode node in userIdList)
+        {
+            Debug.Log(node.ParseXmlNode<string>("user_id"));
+            Debug.Log(node.ParseXmlNode<string>("user_pwd"));
+        }
+
+        Debug.Log("=================================================");
     }
+    
 }
